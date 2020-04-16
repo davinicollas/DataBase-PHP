48d000eecd0c1eaba0109e5eeda5639c734b2548
davinicollas <davi.nicollas@hotmail.com>
2020-04-16 20:00:03 -0300

    delete update

--- class/usuario.php
+++ class/usuario.php
@@ -40,19 +40,61 @@
     $sql =  new Config();
     $results =  $sql->select("SELECT * FROM cad_usuario where id = :ID", array(":ID"=>$id));
     if(count($results) > 0 ){
-      $row  =  $results[0];
-      $this->setId($row['id']);
-      $this->setCad_registro(new DateTime($row['cad_registro']));
-      $this->setCad_senha($row['cad_senha']);
-      $this->setCad_name($row['cad_name']);
+      $this->Setdata($results[0]);
     }
   }
+  public static function getList(){
+    $sql = new Config();
+    return $sql->select("SELECT * FROM cad_usuario ORDER BY cad_name");
+  }
+  public static function search($login){
+    $sql = new Config();
+    return $sql->select("SELECT * FROM cad_usuario where cad_name LIKE :SEARCH  ORDER BY cad_name", array(
+      ':SEARCH'=>"%".$login."%"
+    ));
+  }
+  public function login($login, $password){
+    $sql = new Config();
+    $results = $sql->select("SELECT * FROM cad_usuario where cad_name = :LOGIN AND cad_senha = :PASSWORD ", array(
+      ':LOGIN'=>$login,
+      ':PASSWORD'=> $password));
+    if(count($results) > 0 ){
+      $this->Setdata($results[0]);
+  }else{
+    throw new Exception("Login ou senha invalida"); 
+  }
+}
+public function Setdata($data){
+  $this->setId($data['id']);
+  $this->setCad_registro(new DateTime($data['cad_registro']));
+  $this->setCad_senha($data['cad_senha']);
+  $this->setCad_name($data['cad_name']);
+}
+public function insert(){
+  $sql = new Config();
+  $results = $sql->select("INSERT INTO cad_usuario (cad_name, cad_senha)
+  VALUES (:LOGIN ,:PASSWORD )", array(':LOGIN'=>$this->getCad_name(),
+   ':PASSWORD' =>$this->getCad_senha()
+  ));
+  if(count($results)>0){
+    $this->Setdata($results[0]);
+  }
+}
+public function update($login , $password){
+  $this->setCad_name($login);
+  $this->getCad_senha($password);
+  $sql = new Config();
+  $sql->query("UPDATE cad_usuario SET cad_name = :LOGIN, cad_senha = :PASSWORD WHERE id = :ID" , array(     ':LOGIN'=>$this->getCad_name(),
+    ':PASSWORD' =>$this->getCad_senha(),
+    ':ID'=>$this->getId()
+  ));
+}
   public function __toString(){
     return json_encode(array(
       "id"=>$this->getId(),
       "cad_name"=>$this->getCad_name(),
       "cad_senha"=>$this->getCad_senha(),
-      "cad_registro"=>$this->getCad_registro()->format("d/m/Y H:i:s"),
+      "cad_registro"=>$this->getCad_registro()->format('Y-m-d H:i:s'),
     ));
   }
  }
--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php 
 
 require_once("include.php");
-
+/*
 $root = new Usuario();
 $root->getBYid(2);
 echo $root;
+
+//Lista usuario
+$lista = Usuario::getList();
+echo json_encode($lista);
+
+//Filtrar* 
+$search = Usuario::search('da');
+echo json_encode($search);
+
+//Logar usando dados corretos
+$usuario_login = new Usuario();
+$usuario_login->login("davi nicollas", "15978");
+echo $usuario_login;
+// update usuario
+$aluno = new Usuario();
+$aluno->getBYid(8);
+$aluno->update('professor', 'wueiqieyqiueyqi');
+echo $aluno;
+*/
+$aluno = new Usuario();
+$aluno->setCad_name('davi nicollas pp');
+$aluno->setCad_senha('12345432');
+$aluno->insert();
+echo $aluno;
